92c32b7cdf61d9496cf3a5ca0816a8543857629c
Shuocheng Wang <wangshch5@outlook.com>
2020-12-28 20:07:38 +0800

    Refactor client output and loop num

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,15 @@
 extern crate libc;
 use std::time::Instant;
+use std::process;
 use errno::errno;
 
 fn main() {
-    println!("[client] client app started...");
+    let pid = process::id();
+    println!("[client {}] client app started...", pid);
 
     let socket_fd = unsafe { libc::socket(libc::AF_INET, libc::SOCK_STREAM, 0) };
     if socket_fd < 0 {
-        println!("[client] create socket failed, ret: {}", socket_fd);
+        println!("[client {}] create socket failed, ret: {}", pid, socket_fd);
         return;
     }
 
@@ -26,7 +28,7 @@ fn main() {
         )
     };
     if ret < 0 {
-        println!("[client] connect failed, ret: {}", ret);
+        println!("[client {}] connect failed, ret: {}", pid, ret);
         unsafe {
             libc::close(socket_fd);
         }
@@ -36,27 +38,27 @@ fn main() {
     let mut buf = vec![0u8; 2048];
     let mut cnt = 0;
     let start = Instant::now();
-    while cnt < 20000 {
+    loop {
         let mut ret =
             unsafe { libc::write(socket_fd, buf.as_ptr() as *const libc::c_void, buf.len()) };
         if ret < 0 {
-            println!("[client] write failed, ret: {}, cnt: {}, error {}: {}", ret, cnt, errno().0, errno());
+            println!("[client {}] write failed, ret: {}, cnt: {}, error {}: {}", pid, ret, cnt, errno().0, errno());
             unsafe {
                 libc::close(socket_fd);
             }
             return;
         }
-        // println!("[client] write {} bytes, want write {} bytes", ret, buf.len());
+        // println!("[client {}] write {} bytes, want write {} bytes", pid, ret, buf.len());
 
         if ret < buf.len() as isize {
-            println!("[client] write the rest buffer... {}...{}", ret, buf.len());
+            println!("[client {}] write the rest buffer... {}...{}, iter: {}", pid, ret, buf.len(), cnt);
             let mut cur_ptr = unsafe { buf.as_ptr().add(ret as usize) };
             let mut cur_len = buf.len() - ret as usize;
             while cur_len > 0 {
                 let next_ret =
                     unsafe { libc::write(socket_fd, cur_ptr as *const libc::c_void, cur_len) };
                 if next_ret < 0 {
-                    println!("[client] write failed, ret: {}", ret);
+                    println!("[client {}] write failed, ret: {}", pid, ret);
                     unsafe {
                         libc::close(socket_fd);
                     }
@@ -69,23 +71,23 @@ fn main() {
 
         ret = unsafe { libc::read(socket_fd, buf.as_mut_ptr() as *mut libc::c_void, buf.len()) };
         if ret < 0 {
-            println!("[client] read failed, ret: {}, cnt: {}, error {}: {}", ret, cnt, errno().0, errno());
+            println!("[client {}] read failed, ret: {}, cnt: {}, error {}: {}", pid, ret, cnt, errno().0, errno());
             unsafe {
                 libc::close(socket_fd);
             }
             return;
         }
-        // println!("[client] read {} bytes, want read {} bytes", ret, buf.len());
+        // println!("[client {}] read {} bytes, want read {} bytes", pid, ret, buf.len());
 
         if ret < buf.len() as isize {
-            println!("[client] read the rest buffer... {}...{}", ret, buf.len());
+            println!("[client {}] read the rest buffer... {}...{}, iter：{}", pid, ret, buf.len(), cnt);
             let mut cur_ptr = unsafe { buf.as_mut_ptr().add(ret as usize) };
             let mut cur_len = buf.len() - ret as usize;
             while cur_len > 0 {
                 let next_ret =
                     unsafe { libc::read(socket_fd, cur_ptr as *mut libc::c_void, cur_len) };
                 if next_ret < 0 {
-                    println!("[client] read failed, ret: {}", ret);
+                    println!("[client {}] read failed, ret: {}", pid, ret);
                     unsafe {
                         libc::close(socket_fd);
                     }
@@ -97,11 +99,16 @@ fn main() {
         }
 
         cnt += 1;
+        if cnt % 100000 == 0 {
+            let duration = start.elapsed();
+            println!("[client {}] Time is {:?}, iters: {}", pid, duration, cnt);
+        }
+        if cnt == 1000000 {
+            break;
+        }
     }
-    let duration = start.elapsed();
-    println!("[client] Time is {:?}, iters: {}", duration, cnt);
 
-    println!("[client] close and exit");
+    println!("[client {}] close and exit", pid);
     unsafe {
         libc::close(socket_fd);
     }
